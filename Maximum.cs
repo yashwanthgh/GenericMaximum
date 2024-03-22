61f6a5043f0d5530cc25525afbb8ef4f6e6ecff8
yashwanthgh <yugnasura@gmail.com>
2024-03-22 08:34:32 +0530

    Added the functionality to check maximum for any datatype

--- Maximum.cs
+++ Maximum.cs
@@ -11,15 +11,19 @@ namespace GenericMaximum
         public static void Main(string[] args)
         {
             Maximum maximum = new Maximum();
+            Console.WriteLine("Integer type values");
             Console.WriteLine(maximum.FindMaximum(9, 8, 7));
             Console.WriteLine(maximum.FindMaximum(6, 11, 2));
             Console.WriteLine(maximum.FindMaximum(2, 7, 99));
+            Console.WriteLine("Float type values");
+            Console.WriteLine(maximum.FindMaximum(7.9, 8.7, 7.125));
+            Console.WriteLine(maximum.FindMaximum(6.6, 11.11, 2.9));
+            Console.WriteLine(maximum.FindMaximum(9.7, 9.8, 9.9));
         }
 
-
-        public int FindMaximum(int number1, int number2, int number3)
+        public T FindMaximum<T>(T number1, T number2, T number3) where T : IComparable<T>
         {
-            int max = number1;
+            T max = number1;
 
             // Using CompairTo function of an Object
             if (number2.CompareTo(max) > 0) { max = number2; }
