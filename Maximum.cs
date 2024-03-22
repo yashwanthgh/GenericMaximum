b6ca0a938badf750b445d872066568b5789f752d
yashwanthgh <yugnasura@gmail.com>
2024-03-22 08:29:28 +0530

    Functionality to find maximum in given integer

---
+++ Maximum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMaximum
+{
+    public class Maximum
+    {
+        public static void Main(string[] args)
+        {
+            Maximum maximum = new Maximum();
+            Console.WriteLine(maximum.FindMaximum(9, 8, 7));
+            Console.WriteLine(maximum.FindMaximum(6, 11, 2));
+            Console.WriteLine(maximum.FindMaximum(2, 7, 99));
+        }
+
+
+        public int FindMaximum(int number1, int number2, int number3)
+        {
+            int max = number1;
+
+            // Using CompairTo function of an Object
+            if (number2.CompareTo(max) > 0) { max = number2; }
+            if (number3.CompareTo(max) > 0) { max = number3; }
+            return max;
+        }
+    }
+}
